10e3f98d33f2c4ca357d1cafa0b4bb2a4c63c16a
SemWint <113989932+SemWint@users.noreply.github.com>
2026-01-23 16:00:10 +0100

    initialize project

---
+++ SemaxWeb/src/App.tsx
@@ -0,0 +1,16 @@
+import styles from './App.module.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div className={styles.container}>
+        <img className={styles.logo} src="./public/Semax_Logo.svg" alt="Semax Logo" width={200} />
+        Semax Website
+      </div>
+    </>
+  )
+}
+
+export default App
