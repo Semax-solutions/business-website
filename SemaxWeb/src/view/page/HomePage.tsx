fb5bd7aff9e181ccef13229145f42aa2c2ddc74f
maxie030 <max.verheul@student.hu.nl>
2026-01-25 18:43:02 +0100

    Made home text.

--- SemaxWeb/src/view/page/HomePage.tsx
+++ SemaxWeb/src/view/page/HomePage.tsx
@@ -4,8 +4,15 @@ const HomePage = () => {
     return (
         <>
             <div className={classes.container}>
-                <img className={classes.logo} src="/Semax_Logo.svg" alt="Semax Logo" width={200} />
-                Semax Website
+                <div className={classes.innerContainer}>
+                    <img className={classes.logo} src="/Semax_Logo.svg" alt="Semax Logo" width={200} />
+                    <h1>SEMAX SOLUTIONS</h1>
+                    <p>
+                        At Semax Solutions, we design and build custom software that fits your business.<br></br>
+                        No generic tools, just powerful technology tailored to your needs.
+                    </p>
+                </div>
+
             </div>
         </>
     )
