079b53d8a33e7ab569cf44555e3b02e15f01c1ba
maxie030 <max.verheul@student.hu.nl>
2026-01-28 14:38:38 +0100

    Added arrow down and when clicked on this arrow you go to the next section.

--- SemaxWeb/src/view/page/HomePage.tsx
+++ SemaxWeb/src/view/page/HomePage.tsx
@@ -1,20 +1,34 @@
+import { useRef } from 'react'
 import classes from './css/HomePage.module.css'
 
 const HomePage = () => {
-    return (
-        <>
-            <div className={classes.container}>
-                <div className={classes.innerContainer}>
-                    <img className={classes.logo} src="/Semax_Logo.svg" alt="Semax Logo" width={200} />
-                    <h1>SEMAX SOLUTIONS</h1>
-                    <p>
-                        At Semax Solutions, we design and build custom software that fits your business.<br></br>
-                        No generic tools, just powerful technology tailored to your needs.
-                    </p>
-                </div>
+    const servicesRef = useRef<null | HTMLElement>(null)
 
-            </div>
-        </>
+    return (
+        <main>
+            <section className={classes.contentRow}>
+                <div className={classes.container}>
+                    <div className={classes.innerContainer}>
+                        <img className={classes.logo} src="/Semax_Logo.svg" alt="Semax Logo" width={200} />
+                        <h1>SEMAX SOLUTIONS</h1>
+                        <p>
+                            At Semax Solutions, we design and build custom software that fits your business.<br></br>
+                            No generic tools, just powerful technology tailored to your needs.
+                        </p>
+                    </div>
+                    <div className={`${classes.arrowDownContainer} ${classes.arrow}`} onClick={() => servicesRef.current?.scrollIntoView({behavior: 'smooth'})}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <path d="M4 8L12 16L20 8" stroke="#8B8B8B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className={classes.chevron}/>
+                        </svg>
+                    </div>
+                </div>
+            </section>
+
+            <section ref={servicesRef} className={`${classes.contentRow} ${classes.whiteBackground}`}>
+
+            </section>
+            
+        </main>
     )
 }
 
