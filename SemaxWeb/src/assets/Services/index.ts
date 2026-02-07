2415f68b8ca0c76e9a3417a83aa68379620d908a
SemWint <113989932+SemWint@users.noreply.github.com>
2026-02-07 02:34:34 +0100

    added api-icon.svg

--- SemaxWeb/src/assets/Services/index.ts
+++ SemaxWeb/src/assets/Services/index.ts
@@ -1,2 +1,3 @@
 export { default as CodeIcon } from './code-icon.svg?react';
 export { default as DbIcon } from './db-icon.svg?react';
+export { default as ApiIcon } from './api-icon.svg?react';
--- SemaxWeb/src/view/page/HomePage.tsx
+++ SemaxWeb/src/view/page/HomePage.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react'
 import classes from './css/HomePage.module.css'
 import Card from '../components/card/Card'
-import { CodeIcon, DbIcon } from '../../assets/Services';
+import { CodeIcon, DbIcon, ApiIcon } from '../../assets/Services';
 import { useLocation } from 'react-router-dom';
 
 const HomePage = () => {
@@ -47,7 +47,7 @@ const HomePage = () => {
                     <div className={classes.cardContainer}>
                         <Card icon={<CodeIcon />} title='Software Development' description='From dynamic, responsive front-end interfaces using JavaScript, TypeScript, HTML, and CSS to powerful back-end solutions in Java, C#, and SQL, we build websites and web tailored to your needs.'/>
                         <Card icon={<DbIcon />} title='IT Strategy & Architecture' description='Strategic planning and architectural design to align your technology infrastructure with business objectives. We create scalable, secure and efficient IT code that support your long-term growth and digital transformation.'/>
-                        <Card icon={<DbIcon />} title='Data & Api Integration' description='Seamlessly connect your systems and applications through robust APIs and data integrations and automated data pipelines. We enable real-time data synchronization, streamline workflows, and unlock the full potential of your business data.'/>
+                        <Card icon={<ApiIcon />} title='Data & Api Integration' description='Seamlessly connect your systems and applications through robust APIs and data integrations and automated data pipelines. We enable real-time data synchronization, streamline workflows, and unlock the full potential of your business data.'/>
                         <Card icon={<DbIcon />} title='Management Services' description='Comprehensive IT management and support services to keep your systems running smoothly. From monitoring and maintenance to security updates and performance optimization, we ensure your technology infrastructure operates at peak efficiency.'/>
                         <Card icon={<DbIcon />} title='IT Consulting' description='Expert guidance and strategic advice to navigate complex technology decisions. Our consultants provide actionable insights on digital transformation, technology selection, security best practices, and optimization strategies to drive business success.'/>
                     </div>
